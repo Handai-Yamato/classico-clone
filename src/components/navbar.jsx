27f40acb385c9d8161154c046ee0c7e86c25e3a8
Handai-Yamato <y,handai@crmlab.jp>
2023-04-12 18:20:06 +0900

    add ドロワーメニューの実装#1

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "@/styles/components/Navbar.module.scss";
 
 export default function Navbar() {
-  return <nav>header</nav>;
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  function toggleDrawer() {
+    setIsDrawerOpen(!isDrawerOpen);
+  }
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.gnav}>
+        <div
+          className={`${styles.hamburgerMenu} ${isDrawerOpen ? styles.close : ""}`}
+          onClick={toggleDrawer}
+        >
+          <span className={`${styles.hamburgerIcon} ${isDrawerOpen ? styles.active : ""}`}></span>
+          <span className={`${styles.hamburgerIcon} ${isDrawerOpen ? styles.active : ""}`}></span>
+          <span className={`${styles.hamburgerIcon} ${isDrawerOpen ? styles.active : ""}`}></span>
+        </div>
+
+        <div className={`${styles.drawerMenu} ${isDrawerOpen ? styles.show : ""}`}>
+          <ul>
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }

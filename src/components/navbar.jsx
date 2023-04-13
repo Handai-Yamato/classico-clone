36149da6c9be8161c7a1346cd3977e60c86f2cd7
Handai-Yamato <y,handai@crmlab.jp>
2023-04-13 19:48:20 +0900

    add ナビゲーションバーの実装#1

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import { PrimaryButton } from "@/components/button";
+
 import styles from "@/styles/components/Navbar.module.scss";
 
 export default function Navbar() {
@@ -9,7 +14,18 @@ export default function Navbar() {
 
   return (
     <header className={styles.header}>
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          className={styles.logo}
+          width={123}
+          height={27}
+          priority
+        />
+      </Link>
       <nav className={styles.gnav}>
+        {/* ドロワーメニュー */}
         <div
           className={`${styles.hamburgerMenu} ${isDrawerOpen ? styles.close : ""}`}
           onClick={toggleDrawer}
@@ -26,6 +42,28 @@ export default function Navbar() {
             <li>Menu Item</li>
           </ul>
         </div>
+
+        {/* PCメニュー */}
+        <ul className={styles.gnavList}>
+          <li className={styles.gnavItem}>
+            <Link href="/">ホーム</Link>
+          </li>
+          <li className={styles.gnavItem}>
+            <Link href="/about">会社情報</Link>
+          </li>
+          <li className={styles.gnavItem}>
+            <Link href="/renovate">リノベーションする</Link>
+          </li>
+          <li className={styles.gnavItem}>
+            <Link href="/works">施工事例</Link>
+          </li>
+          <li className={styles.gnavItem}>
+            <Link href="/access">アクセス</Link>
+          </li>
+          <li className={`${styles.gnavItem} ${styles.contactButton}`}>
+            <PrimaryButton href="/contact" text="お問合せ" />
+          </li>
+        </ul>
       </nav>
     </header>
   );
---
+++ src/pages/renovate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const renovate = () => {
+  return <div>renovate</div>;
+};
+
+export default renovate;

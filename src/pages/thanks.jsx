38afe58c85b23c45462eeeeabc76b1c5aa69cf5a
Handai-Yamato <y,handai@crmlab.jp>
2023-05-16 10:55:37 +0900

    add サンクスページ#26

--- src/pages/thanks.jsx
+++ src/pages/thanks.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+// component
+import NestedLayout from "@/components/layout/nested-layout";
+// styles
+import styles from "@/styles/Thanks.module.scss";
 
 const Thanks = () => {
-  return <div>thanks</div>;
+  return (
+    <NestedLayout
+      title="お問い合わせありがとうございます"
+      pageName="お問い合わせありがとうございます"
+    >
+      <div className={styles.sectionContainer}>
+        <h3 className={styles.title}>お問い合わせを受け付けました。</h3>
+        <p className={styles.text}>
+          ご記入いただいた情報は無事送信されました。
+          <br />
+          確認のためお客様へ自動返信メールをお送りさせていただきました。
+          <br />
+          お問い合わせ頂き、ありがとうございました。
+        </p>
+      </div>
+    </NestedLayout>
+  );
 };
 
 export default Thanks;
